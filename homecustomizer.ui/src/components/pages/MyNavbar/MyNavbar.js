1adc4d6333b59041c145e39204f64488e442b26b
Jbkey1983 <jbk_1_tn@yahoo.com>
2020-07-18 11:17:53 -0500

    routes work

---
+++ homecustomizer.ui/src/components/pages/MyNavbar/MyNavbar.js
@@ -0,0 +1,14 @@
+import './MyNavbar.scss';
+import React from 'react';
+
+class MyNavbar extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>MyNavbar</h1>
+      </div>
+    );
+  }
+}
+
+export default MyNavbar;
