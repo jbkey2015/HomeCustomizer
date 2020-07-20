86748d5e6bf0365a271d369022fb4666d4e0832d
Jbkey1983 <jbk_1_tn@yahoo.com>
2020-07-20 09:58:05 -0500

    added navbar and connected routing to navbar

--- homecustomizer.ui/src/components/pages/MyNavbar/MyNavbar.js
+++ homecustomizer.ui/src/components/pages/MyNavbar/MyNavbar.js
@@ -4,9 +4,19 @@ import React from 'react';
 class MyNavbar extends React.Component {
   render() {
     return (
-      <div>
-        <h1>MyNavbar</h1>
-      </div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/homeCustomizer">Home Customizer</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="/home">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="/siding">Siding</a>
+            <a class="nav-item nav-link" href="/shutters">Shutters</a>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
