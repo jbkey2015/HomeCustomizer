6375194fedfd1303f51dfebe318d317cd9ba66cf
Jbkey1983 <jbk_1_tn@yahoo.com>
2020-07-24 12:33:53 -0500

    home page backend setup

---
+++ HomeCustomizer/Controllers/CommunityController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HomeCustomizer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommunityController : ControllerBase
+    {
+        // GET: api/<CommunityController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CommunityController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CommunityController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CommunityController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CommunityController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
