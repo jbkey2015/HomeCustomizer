2f28519c0ca5f77c7753b3ce0059978d33ff0c98
Jbkey1983 <jbk_1_tn@yahoo.com>
2020-08-03 20:59:46 -0500

    WIP

--- homecustomizer.ui/src/components/pages/HomeCustomizer/HomeCustomizer.js
+++ homecustomizer.ui/src/components/pages/HomeCustomizer/HomeCustomizer.js
@@ -9,28 +9,9 @@ class HomeCustomizer extends React.Component {
     colorSearch: "",
     resultSearch: [],
     wantsToSearch: false,
-    toggleMainContainer:false,
-    query: ''
   }
 
-  // colorFilter = (query) => {
-  //   const value = $(query.target).val().toLowerCase();
-  //   const colors = $(`https://localhost:44334/api/colorBoard/${query}`);
-  //   colors.hide();
-  //   if (value !== '') {
-  //     colors.filter(this.colorBoards).show();
-  //     console.warn(this.colorBoards);
-  //   } else {
-  //     colors.show();
-  //   }
-  // };
   
-  // searchFilter = () => {
-  //   $('#myInput').on('keyup', (e) => {
-  //     this.colorFilter(e);
-  //   });
-  // };
-
   componentDidMount() {
     colorBoardData.getAllColorBoards()
       .then((colorBoards) => this.setState({ colorBoards }))
@@ -48,45 +29,18 @@ class HomeCustomizer extends React.Component {
     this.setState({ colorSearch: colorSearch })
   }
 
-
-//   toggle = () => {
-//     this.setState({toggleMainContainer:true})
-//   }
-
-//   endToggle = () => {
-//     this.setState({toggleMainContainer:false})
-
-//   }
-
-//   isEmptyOrSpaces = (str) => {
-//     return str === null || str.match(/^ *$/) !== null;
-// }
-
-//   handleOnInputChange = e => {
-//     if(this.isEmptyOrSpaces(e.target.value)){
-//       this.setState({query: ''})
-//       this.endToggle()
-//       return
-//     }
-//     const stateToChange ={}
-//     stateToChange[e.target.id] = e.target.value;
-//     this.setState(stateToChange)
-//     this.toggle()
-//   }
-
   render() {
     const { colorBoards } = this.state;
     return (
       <div className="AllColorBoards">
         <form class="form-inline my-2 my-lg-0">
           <input id="myInput" class="form-control mr-sm-2" value={this.state.colorSearch} onChange={this.dynamicSearch} type="search" placeholder="Search" aria-label="Search"></input>
-          {/* <button class="btn btn-outline-primary my-2 my-sm-0" onClick={this.searchBtnEvt} type="submit">Search</button> */}
         </form>
         {
           !this.state.wantsToSearch ?
           <div>
             <div>
-              <div className="shutters d-flex flex-wrap">
+              <div className="colorBoards d-flex flex-wrap">
               {colorBoards.map((colorBoard) => <ColorBoard key={colorBoard.id} colorBoard={colorBoard} />)}
               </div>
             </div>
